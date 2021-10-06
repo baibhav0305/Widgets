d8cb7cdae7726a6be18cfd9e66b9446851b62873
Baibhav <baibhav030501@gmail.com>
2021-10-06 19:35:28 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
+import Translate from "./Translate";
+import Route from "./Route";
+import Navbar from "./Navbar";
 
 const items = [
-    {
-        title: "What is React?",
-        content: "React is a front end javascript framework"
-    },
-    {
-        title: "Why use React?",
-        content: "React is a favourite javascript library among engineers"
-    },
-    {
-        title: "How do you use React?",
-        content: "You use React by creating components"
-    }
-]
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite javascript library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of Blue",
+    value: "blue",
+  },
+];
+
+// const showAccordion = () => {
+//   if (window.location.pathname === "/") {
+//     return <Accordion items={items} />;
+//   }
+// };
+
+// const showList = () => {
+//   if (window.location.pathname === "/list") {
+//     return <Search />;
+//   }
+// };
+
+// const showDropdown = () => {
+//   if (window.location.pathname === "/dropdown") {
+//     return <Dropdown />;
+//   }
+// };
+
+// const showTranslate = () => {
+//   if (window.location.pathname === "/translate") {
+//     return <Translate />;
+//   }
+// };
 
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Accordion items={items}/>
+      {/* <Accordion items={items}/> */}
+
+      {/* <Search /> */}
+
+      {/* <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null} */}
+
+      {/* <Translate /> */}
+
+      <Navbar />
+
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
